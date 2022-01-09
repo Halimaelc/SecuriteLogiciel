b9ade2734a56680c7808e1aafb1c7cec50e9f274
Halimaelc <halimaelcadi2@gmail.com>
2022-01-10 00:39:09 +0100

    commit

---
+++ src/main/java/tp2/User/UserService.java
@@ -0,0 +1,23 @@
+package tp2.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    //methode pour recuperer tout les comptes
+    public List<User> getAll()
+    {
+        List<Object> cartes= carterepo.afficherall();
+        return cartes;
+    }
+}
